692ab49aef68f8655bf9a20246ae1321331b6831
Curtis Delicata <curtis.delicata@outlook.com>
2018-05-22 02:01:41 +0100

    Remove pivot parameters

--- resources/assets/js/pages/workorders/Create.vue
+++ resources/assets/js/pages/workorders/Create.vue
@@ -39,17 +39,17 @@
 
 <script>
 
-    import VueFormSs from '../../components/enso/vueforms/VueFormSs.vue';
-    import VueSelect from '../../components/enso/select/VueSelect.vue';
+import VueFormSs from '../../components/enso/vueforms/VueFormSs.vue';
+import VueSelect from '../../components/enso/select/VueSelect.vue';
 
-    export default {
-        components: { VueFormSs, VueSelect },
+export default {
+    components: { VueFormSs, VueSelect },
 
-        data() {
-            return {
-                pivotParams: { customers: { id: null } },
-            };
-        },
-    };
+    data() {
+        return {
+
+        };
+    },
+};
 
 </script>
